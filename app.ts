2008421632ea14bd27353a1f4736316c20b10b61
Xslead <celalmert2004@gmail.com>
2025-08-12 21:21:14 +0300

    fix: github pages deployment, asset paths, tsconfig

---
+++ app.ts
@@ -0,0 +1,6 @@
+import { UIController } from "./src/ui/UIController";
+import "../styles.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  new UIController().init();
+});
--- src/app.ts
+++
@@ -1,6 +0,0 @@
-import { UIController } from "./ui/UIController";
-import "../styles.css";
-
-document.addEventListener("DOMContentLoaded", () => {
-  new UIController().init();
-});
